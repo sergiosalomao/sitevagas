5b502fe9becfddaa7c9370b717d7a46bf1c017b6
Sergio Salomao <salomao.leite@fabricainfo.com>
2019-12-03 17:10:47 -0300

    ajustes iniciais

--- routes/api.php
+++ routes/api.php
@@ -2,27 +2,11 @@
 
 use Illuminate\Http\Request;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
 
-// Route::middleware('auth:api')->get('/user', function (Request $request) {
-//     return $request->user();
-// });
-
-
-Route::group(['middleware' => ['jwt.verify']], function() {
-    Route::apiResource('empresas','EmpresaController');
+Route::group(['middleware' => ['jwt.verify']], function () {
+    Route::apiResource('empresas', 'EmpresaController');
 });
 
-Route::apiResource('vagas','VagaController');
+Route::apiResource('vagas', 'VagaController');
 Route::post('login', 'AuthController@login');
 Route::post('register', 'AuthController@register');
-
